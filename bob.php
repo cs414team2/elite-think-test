2a011beb8cbd02b7766e03d83476db8f9698b955
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-08 21:32:12 -0600

    stuff

---
+++ bob.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+	<title>Team02 :: CS 414</title>
+	<style type="text/css">
+			h1, h2 {
+				text-align: center;
+			}
+			img {
+			    border-style: inset;
+				border-radius: 20px;
+			}
+			h3 {
+				text-align: center;
+			}
+			div {
+				background-color: rgba(255,255,255,0.8);
+				border-radius: 20px;
+				margin-top: 20px;
+				margin-right: 20px;
+				margin-bottom: 20px;
+				margin-left: 20px;
+				padding-left: 20px;
+			}
+			body {
+				background-image:url(images/glow.png);
+				background-attachment: fixed;
+				background-size: cover;
+			}
+			a {
+			 -webkit-transition: all .5s;
+			 -moz-transition: all .5s;
+			 transition: all .5s;
+			}
+			a:link{
+			   color: #000;
+			}
+			a:visited{
+			   color: #000;
+			}
+			a:hover{
+			   color: #F00;
+			   padding-left: 5px;
+			}
+			a:active{
+			   color: #F00;
+			}
+		</style>
+</head>
+
+<body style="background: #FF0000">
+	<h1>The Brogrammers</h1>
+	
+	<div style="width: 100%">
+		<form>
+			User name:<br>
+			<input type="text" name="username"><br><br>
+			Password:<br>
+			<input type="password" name="password">
+			<br><br>
+			<input type="submit" value="Login">
+		</form>
+		<a href="index.php" class="button big">Back To Main Page</a>
+	</div>
+</body>
+</html>
