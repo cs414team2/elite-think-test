3c71e88571afeddee14542fec9fbf85526425066
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-08 19:18:09 -0600

    First commit

---
+++ studentHome.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Elite Outline</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Elite Testing application" />
+		<meta name="keywords" content="testing, login, student, teacher, administrator" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrollgress.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.slidertron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.php">Elite Team</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li>
+							<a href="" class="icon fa-angle-down">Helpful Links</a>
+							<ul>
+								<li>
+									<a href="bob.php">Old Page</a></li>
+									<li><a href="file://csmain/Classes/CS_414/Team02/outline.php">Page Outline</a></li>
+								</li>							
+							</ul>
+						</li>
+						<li><a href="https://www.google.com/drive/">Google Drive</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Main -->
+			<section id="main" class="wrapper style1">
+				<header class="major">
+					<h2>Student Home</h2>
+					<p>This page has some cool things.</p>
+				</header>
+				<div class="container">
+						
+					<!-- Content -->
+						<section id="content">
+							<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+							<h3>Heading for the below feature</h3>
+							<p>Put the different tables here.</p>
+						</section>
+								
+				</div>
+			</section>
+			
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+					<li><a href="#" class="icon fa-envelope"><span class="label">Envelope</span></a></li>
+				</ul>
+				<ul class="menu">
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">User Manual</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</footer>
+
+	</body>
+</html>
