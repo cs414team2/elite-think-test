23bbd1930082ffe0298b1fd0f5660e7f007255a9
davis34c <jdavis2116@students.pcci.edu>
2016-02-15 15:56:21 -0600

    anything

--- views/administrator/adminHome.php
+++ views/administrator/adminHome.php
@@ -5,39 +5,27 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="Elite Testing application" />
 		<meta name="keywords" content="testing, login, student, teacher, administrator" />
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/jquery.scrollgress.min.js"></script>
-		<script src="js/jquery.scrolly.min.js"></script>
-		<script src="js/jquery.slidertron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-xlarge.css" />
-		</noscript>
+
 
 	</head>
 	<body>
 
 		<?php 
 			// Includes the Header for the page
-			require_once('header.php');
+			require_once('../header.php');
 		?>
 
 		<!-- Main -->
 			<section id="main" class="wrapper style1">
 				<header class="major">
-					<h2>Administration Page</h2>
+					<h2>Heading Here</h2>
 					<p>This page has some cool things.</p>
 				</header>
 				<div class="container">
 						
 					<!-- Content -->
 						<section id="content">
-							<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+							<a href="#" class="image fit"><img src="../images/pic07.jpg" alt="" /></a>
 							<h3>Heading for the below feature</h3>
 							<p>Put the different tables here.</p>
 						</section>
@@ -47,7 +35,7 @@
 			
 		<?php 
 			// Includes the Footer for the page
-			require_once('footer.php');
+			require_once('../footer.php');
 		?>
 
 	</body>
