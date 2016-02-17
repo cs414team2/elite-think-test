d1f98d33de93accaf8a0f26d5fb1b5375ec1a278
daniel-swan <dswan0116@students.pcci.edu>
2016-02-16 21:04:28 -0600

    refined the workings of adding a student and added error messages to adminStudentManager

--- views/administrator/adminStudentsManager.php
+++ views/administrator/adminStudentsManager.php
@@ -12,22 +12,57 @@ if (isset($_SESSION['credentials'])) {
 						var firstname = $("#firstname").val();
 						var lastname = $("#lastname").val();
 						var email = $("#emailAddress").val();
-					
-						$.ajax({
-							url: "ajax/add_student.php",
-							type: "POST",
-							data: { password: password,
-									firstname: firstname,
-									lastname: lastname,
-									email: email
-								  }
-						});
+						var validated = true;
 						
-						$("#password").val(\'\');
-						$("#firstname").val(\'\');
-						$("#lastname").val(\'\');
-						$("#emailAddress").val(\'\');
+						if (jQuery.trim(password).length <= 0) {
+							$("#add_password_err").show();
+							validated = false;
+						}
+						if (jQuery.trim(firstname).length <= 0) {
+							$("#add_first_err").show();
+							validated = false;
+						}
+						if (jQuery.trim(lastname).length <= 0) {
+							$("#add_last_err").show();
+							validated = false;
+						}
+						if (jQuery.trim(email).length <= 0) {
+							$("#add_email_err").show();
+							validated = false;
+						}
+					
+						if (validated)
+						{	$.ajax({
+								url: "ajax/add_student.php",
+								type: "POST",
+								data: { password: password,
+										firstname: firstname,
+										lastname: lastname,
+										email: email
+									  }
+							});
+						
+							$("#password").val(\'\');
+							$("#firstname").val(\'\');
+							$("#lastname").val(\'\');
+							$("#emailAddress").val(\'\');
+							
+							location.href = "./?action=admin_student_manager";
+						}
 					})
+					
+					$("#password").keypress(function(){
+						$("#add_password_err").hide();
+					});
+					$("#firstname").keypress(function(){
+						$("#add_first_err").hide();
+					});
+					$("#lastname").keypress(function(){
+						$("#add_last_err").hide();
+					});
+					$("#emailAddress").keypress(function(){
+						$("#add_email_err").hide();
+					});
 				});
 			</script>
 			<section id="main" class="wrapper style1">
@@ -44,12 +79,13 @@ if (isset($_SESSION['credentials'])) {
 									<thead>
 										<tr>
 											<th>ID</th>
+											<th>Password</th>
 											<th>Name</th>
 											<th>Last</th>
 											<th>Email</th>
 										</tr>
 									</thead>
-									<tbody>
+									<tbody >
 										<!-- Load all students -->
 										';
 
@@ -72,12 +108,24 @@ if (isset($_SESSION['credentials'])) {
 									<form>
 									  First name:<br/>
 									  <input type="text" id="firstname" name="firstname">
+									  <p id="add_first_err" style="display:none; color: red;">
+										First name cannot be empty.
+									  </p>
 									  Last name:<br/>
 									  <input type="text" id="lastname" name="lastname">
+									  <p id="add_last_err" style="display:none; color: red;">
+										Last name cannot be empty.
+											</p>
 									  Password:<br/>
 									  <input type="text" id="password" name="password">
+									  <p id="add_password_err" style="display:none; color: red;">
+										Password cannot be empty.
+											</p>
 									  Email:<br/>
-									  <input type="text" id="emailAddress" name="emailAddress">
+									  <input type="text" id="emailAddress" name="emailAddress" >
+									  <p id="add_email_err" style="display:none; color: red;">
+										Email cannot be empty.
+									  </p>
 									</form>
 									 
 									<br />
