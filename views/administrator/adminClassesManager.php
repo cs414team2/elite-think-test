c2d426641d3aa19c3e20acdb522935343ffcb9ce
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-16 19:43:23 -0600

    added drop down list functionality

--- views/administrator/adminClassesManager.php
+++ views/administrator/adminClassesManager.php
@@ -1,5 +1,6 @@
 <?php
 require_once('model/Table.php');
+require_once('model/Admin.php');
 if (isset($_SESSION['credentials'])) {
 	if ($_SESSION['credentials']->is_admin()) {
 		echo'<!-- Main -->
@@ -35,20 +36,22 @@ if (isset($_SESSION['credentials'])) {
 						<a href="#" class="show_hide" rel="#slidingDiv_2">Add a Course</a><br />
 						<div id="slidingDiv_2">
 						<form>
-						  Course name:<br />
-						  <input type="text" name="courseName">
-						  Course Number:<br />
-						  <input type="text" name="courseNumber">
+						    Course name:<br />
+						    <input type="text" name="courseName">
+						    Course Number:<br />
+						    <input type="text" name="courseNumber"> <br />
 						  
-										<div class="row uniform">
-											<div class="12u">
-												<div class="select-wrapper">
-													<select name="Teacher" id="Teacher">
-														<option value="">- Teacher -</option>
-													</select>
-												</div>
-											</div>
-										</div>
+							<div class="row uniform">
+								<div class="12u">
+									<div class="select-wrapper">
+										<select name="Teacher" id="Teacher">
+											<option selected="selected">- Select a Teacher -</option>';
+											$admin = new Admin();
+											$admin->get_teachers();
+								  echo '</select>
+									</div>
+								</div>
+							</div>
 
 						  
 						  <br />
