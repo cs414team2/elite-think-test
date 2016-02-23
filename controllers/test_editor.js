dc3293dcccd2bee8963d5abbf0a964cc68481f94
daniel-swan <dswan0116@students.pcci.edu>
2016-02-23 16:55:45 -0600

    Begun creating code to add tests and edit them.

--- controllers/test_editor.js
+++ controllers/test_editor.js
@@ -1,7 +1,7 @@
 function add_question(test_id, question_type) {
 	$.ajax({
 		url: 'ajax/add_question.php',
-		data { 
+		data: { 
 			test_id: test_id,
 			question_type: question_type
 		}
@@ -12,12 +12,11 @@ function edit_question(question_id, question_type, question_text,
 					   question_weight, question_number, answers) {
 	$.ajax({
 		url: 'ajax/edit_question.php',
-		data {
+		data: {
 			question_id: question_id,
 			question_type: question_type,
 			question_text: question_text,
 			question_weight: question_weight,
-			question_number: question_number,
 			answers: answers
 		}
 	});
@@ -26,6 +25,6 @@ function edit_question(question_id, question_type, question_text,
 function delete_question(question_id) {
 		$.ajax({
 		url: 'ajax/delete_question.php',
-		data { question_id: question_id }
+		data: { question_id: question_id }
 	});
 }
