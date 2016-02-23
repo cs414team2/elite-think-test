c7470537ca603b299bfd6e145a8e03166a8a4d7a
gameking0914 <jschef5509@students.pcci.edu>
2016-02-23 16:08:38 -0600

    Fixed JS crashing order

--- views/teacher/teacherCreateTest.php
+++ views/teacher/teacherCreateTest.php
@@ -24,8 +24,8 @@ if (isset($_SESSION['credentials'])) {
 				<br />
 				
 				<p>Class:</p>
-					<select name="Teacher" id="Teacher">
-												<option selected="selected" value="null">- Select a Teacher -</option>';
+					<select name="Class" id="Class">
+												<option selected="selected" value="null">- Select a Class -</option>';
 												$teacher = new Teacher();
 												$teacher->get_classes_dropdown($_SESSION['credentials']->get_user_id());
 									  echo '</select>
@@ -44,44 +44,7 @@ if (isset($_SESSION['credentials'])) {
 			
 			
 			<div id="my-form-builder" class="content">
-				<div id="users-contain" class="ui-widget">
-					<h1>Existing Users:</h1>
-					<table id="users" class="ui-widget ui-widget-content">
-						<thead>
-							<tr class="ui-widget-header ">
-								<th>Name</th>
-								<th>Email</th>
-								<th>Password</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>John Doe</td>
-								<td>john.doe@example.com</td>
-								<td>johndoe1</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>	
-				<button id="create-user">Create new user</button>
-			</div>
-			
-			<div id="dialog-form" title="Create new Question">
-				<p class="validateTips">All form fields are required.</p>
-				 
-				<form>
-					<fieldset>
-						<label for="name">Name</label>
-						<input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
-						<label for="email">Email</label>
-						<input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
-						<label for="password">Password</label>
-						<input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
-				 
-						<!-- Allow form submission with keyboard without duplicating the dialog button -->
-						<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
-					</fieldset>
-				</form>
+				
 			</div>
 			
 			
