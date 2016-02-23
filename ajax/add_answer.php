3e515a215f385e2517583e02bef5a7071c3f2d25
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-23 16:02:55 -0600

    fixed add empty question

--- ajax/add_answer.php
+++ ajax/add_answer.php
@@ -1,7 +1,13 @@
 <?php
-	if(isset() {
+	if(isset($_REQUEST['question_id'], $_REQUEST['answer_content'], $_REQUEST['is_correct'])) {
+		$question_id = $_REQUEST['question_id'];
+		$answer_content = $_REQUEST['answer_content'];
+		$is_correct = $_REQUEST['is_correct'];
 		
+		$eliteConnection = new mysqli("csweb.studentnet.int", "team2_cs414", "t2CS414", "cs414_team2");
 		
-		
+		$addStatement = $eliteConnection->prepare("CALL add_question(?, ?, ?)") or die($eliteConnection->error);
+		$addStatement->bind_param("ssisi", $question_id, $answer_content, $is_correct);
+		$addStatement->execute();
 	}
 ?>
