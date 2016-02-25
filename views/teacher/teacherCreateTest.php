8460825a7e12056ceb06546231269d4a9f9580e3
davis34c <jdavis2116@students.pcci.edu>
2016-02-25 14:24:29 -0600

    Test creation

--- views/teacher/teacherCreateTest.php
+++ views/teacher/teacherCreateTest.php
@@ -6,6 +6,16 @@ if (isset($_SESSION['credentials'])) {
 		// PUT HTML HERE!
 		echo '
 		<section id="main" class="wrapper style1">
+		
+		    <script language="javascript">
+			function setRadio(obj) 
+			{
+				if(obj.checked == true)
+					obj.checked = false;
+				else
+					obj.checked = true
+			}
+			</script>
 			<header class="major">
 				<h2> Test Creation</h2>
 				<p>Create and Manage Tests</p>
@@ -22,25 +32,81 @@ if (isset($_SESSION['credentials'])) {
 				<a href="https://jqueryui.com/dialog/#default" target="_blank">JQuery Dialog Boxes</a>
 				<br />
 				
+				<section style="text-align:center">
+						<a class="show_hide" rel="#slidingDiv_1" >Multiple Choice</a>
+						<a class="show_hide" rel="#slidingDiv_2" >True or False</a>
+						<a class="show_hide" rel="#slidingDiv_3" >Essay</a><br />
+				</section>
+				
 				
 				<p>Time Limit on test: </p>
 					<input type="number" name="timeLimit">	
 			</div>
-			<div id="my-form-builder" class="content">
-				<form class="questions">
+		    
+			<div id="slidingDiv_1" class="toggleDiv" style="display:none"> 
+			<section id="MultipleChoice">
+				<div id="my-form-builder"  align="center">
 					<h4>Multiple Choice</h4>
-					<label for="mcqEntry">Question Entry</label><br />
-					<input id="mcqEntry" type="text" name="mcqEntry" class="questionStyle"><br />
-					<label for="mcAnswer1">1)</label>
-					<input id="mcAnswer1" type="text" name="mcAnswer1" class="questionStyle"><br />
-					<label for="mcAnswer2">2)</label>
-					<input id="mcAnswer2" type="text" name="mcAnswer2" class="questionStyle"><br />
-					<label for="mcAnswer3">3)</label>
-					<input id="mcAnswer3" type="text" name="mcAnswer3" class="questionStyle"><br />
-					<label for="mcAnswer4">4)</label>
-					<input id="mcAnswer4" type="text" name="mcAnswer4" class="questionStyle"><br />
+					<input id="mcqEntry" type="text" placeholder="Enter a Multiple Choice Question" name="mcqEntry" class="questionStyle">
+					
+					<label for="mcAnswer1"> Choice 1</label>
+					<input id="mcAnswer1" type="text" name="mcAnswer1" class="questionStyle">
+					
+					<label for="mcAnswer2"> Choice 2</label>
+					<input id="mcAnswer2" type="text" name="mcAnswer2" class="questionStyle">
+					
+					<label for="mcAnswer3"> Choice 3</label>
+					<input id="mcAnswer3" type="text" name="mcAnswer3" class="questionStyle">
+					
+					
+					<label for="mcAnswer4"> Choice 4</label>
+					<input id="mcAnswer4" type="text" name="mcAnswer4" class="questionStyle">
+					
+					
 					<button type="button">Submit Question</button>
-				</form>
+				</div>
+			</section>
+			</div>
+			
+			<div id="slidingDiv_2" class="toggleDiv" style="display:none"> 
+			<section id="TorF">
+				
+				<div id="my-form-builder"  align="center">
+					<h4>T/F Question</h4>
+					<form class="4u 12u(2)">
+						<input id="tfqEntry" type="text" placeholder="Enter a T/F Question" name="mfqEntry" class="questionStyle"><br />
+						
+						
+							<input type="radio" id="answerTrue" name="tfButton"  checked>
+							<label for="answerTrue">True</label>
+						
+						
+							<input type="radio" id="answerFalse" name="tfButton" >
+							<label for="answerFalse">False</label>
+							<br>
+							<button type="button">Submit Question</button>
+						
+					</form>
+			</section>
+			</div>
+			
+
+			
+			
+			
+			
+			</div>
+			
+			
+			<div id="slidingDiv_3" class="toggleDiv" style="display:none"> 
+			<section id="essayQuestion">
+				<div id="my-form-builder" align="center">
+					<h4>Essay Question</h4>
+					<input id="eqEntry" type="text" placeholder="Enter a Essay Question" name="mcqEntry" class="questionStyle"><br />
+
+					<button type="button">Submit Question</button>
+				</div>
+			</section>
 			</div>
 			
 			
