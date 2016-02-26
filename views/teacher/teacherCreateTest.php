2185d2532606a5179ec201cf177bec44414d9cac
gameking0914 <jschef5509@students.pcci.edu>
2016-02-25 20:19:35 -0600

    Test page edited

--- views/teacher/teacherCreateTest.php
+++ views/teacher/teacherCreateTest.php
@@ -21,83 +21,100 @@ if (isset($_SESSION['credentials'])) {
 				<p>Create and Manage Tests</p>
 			</header>
 		
-		<div class="testContainer">
-			<div id="sidebar" style="text-align:center">
-				
-				<section style="text-align:center">
-						<button class="show_hide button small fit" rel="#slidingDiv_1" >M/C</button>
-						<button class="show_hide button small fit" rel="#slidingDiv_2" >T/F</button>
-						<button class="show_hide button small fit" rel="#slidingDiv_3" >Essay</button><br />
-				</section>
-				
-				<h4 style="color:white;">Time Limit on test: </h4>
+			<div class="testContainer">
+				<div id="sidebar" style="text-align:center">
+					
+					<section style="text-align:center">
+							<button class="show_hide button small fit" rel="#slidingDiv_1" >M/C</button>
+							<button class="show_hide button small fit" rel="#slidingDiv_2" >T/F</button>
+							<button class="show_hide button small fit" rel="#slidingDiv_3" >Essay</button><br />
+					</section>
+					
+					<h4 style="color:white;">Time Limit on test: </h4>
 					<input type="number" name="timeLimit" style="text-align: center; align:center">	
-			</div>
-		    
-			<div id="slidingDiv_1" class="toggleDiv" style="display:none; max-height:500px;" > 
-			<section id="MultipleChoice">
-				<div id="my-form-builder"  align="left">
-					<h4>Multiple Choice</h4>
-					<input id="mcqEntry" type="text" placeholder="Enter a Multiple Choice Question" name="mcqEntry" class="questionStyle"><br/>
-					<br/>
-					<label for="mcAnswer1" class="questionLabel"> Choice 1</label>
-					<input id="mcAnswer1" type="text" name="mcAnswer1" class="questionStyle"><br/>
+					<br /><br /><br /><br /><br /><br /><br /><br />
 					
-					<label for="mcAnswer2" class="questionLabel"> Choice 2</label>
-					<input id="mcAnswer2" type="text" name="mcAnswer2" class="questionStyle"><br/>
-					
-					<label for="mcAnswer3" class="questionLabel"> Choice 3</label>
-					<input id="mcAnswer3" type="text" name="mcAnswer3" class="questionStyle"><br/>
-					
-					
-					<label for="mcAnswer4" class="questionLabel"> Choice 4</label>
-					<input id="mcAnswer4" type="text" name="mcAnswer4" class="questionStyle"><br/>
-					
-					
-					<button type="button">Submit Question</button>
-					<hr>
+					<button class="show_hide button small fit">Finalize</button>
+					<br />
 				</div>
-			</section>
-			</div>
-			
-			<div id="slidingDiv_2" class="toggleDiv" style="display:none"> 
-				<section id="TorF">
-					
-					<div id="my-form-builder"  align="left" >
-						<h4>T/F Question</h4>
-						<form class="4u 12u(2)">
-							<input id="tfqEntry" type="text" placeholder="Enter a T/F Question" name="mfqEntry" class="questionStyle"><br />
+				
+				<div id="slidingDiv_0" class="toggleDiv" style="display:inline-block; min-width: 40%; float: left;"> 
+					<section id="essayQuestion">
+						<div id="my-form-builder" align="left">
+							<h4>Use links to the left to add a question</h4>
+							<br />
+						</div>
+					</section>
+				</div>
+				
+				<div id="slidingDiv_1" class="toggleDiv" style="display:none; min-width: 40%; float: left;"> 
+					<section id="MultipleChoice">
+						<div id="my-form-builder"  align="left">
+							<h4>Multiple Choice</h4>
+							<input id="mcqEntry" type="text" placeholder="Enter a Multiple Choice Question" name="mcqEntry" class="questionStyle"><br/>
+							<br/>
+							<label for="mcAnswer1" class="questionLabel"> Choice 1</label>
+							<input id="mcAnswer1" type="text" name="mcAnswer1" class="questionStyle"><br/>
+							
+							<label for="mcAnswer2" class="questionLabel"> Choice 2</label>
+							<input id="mcAnswer2" type="text" name="mcAnswer2" class="questionStyle"><br/>
+							
+							<label for="mcAnswer3" class="questionLabel"> Choice 3</label>
+							<input id="mcAnswer3" type="text" name="mcAnswer3" class="questionStyle"><br/>
 							
 							
-								<input type="radio" id="answerTrue" name="tfButton"  checked>
-								<label for="answerTrue" class="questionLabel">True</label>
+							<label for="mcAnswer4" class="questionLabel"> Choice 4</label>
+							<input id="mcAnswer4" type="text" name="mcAnswer4" class="questionStyle"><br/>
 							
 							
-								<input type="radio" id="answerFalse" name="tfButton" >
-								<label for="answerFalse" class="questionLabel">False</label>
-								<br>
-								<button type="button">Submit Question</button>
-							
-						</form>
-				</section>
-			</div>
-			
-			
-			<div id="slidingDiv_3" class="toggleDiv" style="display:none"> 
-			<section id="essayQuestion">
-				<div id="my-form-builder" align="left">
-					<h4>Essay Question</h4>
-					<input id="eqEntry" type="text" placeholder="Enter a Essay Question" name="mcqEntry" class="questionStyle"><br />
+							<button type="button">Submit Question</button>
+							<hr>
+						</div>
+					</section>
+				</div>
+				
+				<div id="slidingDiv_2" class="toggleDiv" style="display:none; min-width: 40%; float: left;"> 
+					<section id="TorF">
+						
+						<div id="my-form-builder"  align="left" >
+							<h4>T/F Question</h4>
+							<form class="4u 12u(2)">
+								<input id="tfqEntry" type="text" placeholder="Enter a T/F Question" name="mfqEntry" class="questionStyle"><br />
+								
+								
+									<input type="radio" id="answerTrue" name="tfButton"  checked>
+									<label for="answerTrue" class="questionLabel">True</label>
+								
+								
+									<input type="radio" id="answerFalse" name="tfButton" >
+									<label for="answerFalse" class="questionLabel">False</label>
+									<br>
+									<button type="button">Submit Question</button>
+								
+							</form>
+					</section>
+				</div>
+				
+				<div id="slidingDiv_3" class="toggleDiv" style="display:none; min-width: 40%; float: left;"> 
+					<section id="essayQuestion">
+						<div id="my-form-builder" align="left">
+							<h4>Essay Question</h4>
+							<input id="eqEntry" type="text" placeholder="Enter a Essay Question" name="mcqEntry" class="questionStyle"><br />
 
-					<button type="button">Submit Question</button>
+							<button type="button">Submit Question</button>
+						</div>
+					</section>
 				</div>
-			</section>
+				
+				<div style="float:right; inline-block; min-width: 60%;"> 
+					<section id="testView">
+						<div id="my-form-builder" align="left" style="padding-left: 200px;">
+							<h4>Test</h4>
+							<br />
+						</div>
+					</section>
+				</div>
 			</div>
-			
-			
-		</div>
-		
-		
 		</section>
 		
 			
