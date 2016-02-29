82bda8a8182d535bb935cb320386e43b3eb34a64
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-29 16:48:50 -0600

    change

--- views/outline.php
+++ views/outline.php
@@ -4,6 +4,7 @@
 	<!-- made a change -->
 	<!-- made a change -->
 	<!-- made a change -->
+	<!-- made a change -->
 		<title>Elite Outline</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="Elite Testing application" />
