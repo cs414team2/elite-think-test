dc4bfd13f37ec06dacf8f56314a1892385058533
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-29 19:28:13 -0600

    changed the way tables load

--- model/Table.php
+++ model/Table.php
@@ -17,7 +17,10 @@
 			if($statement->num_rows > 0){
 				while($record = $statement->fetch_assoc()){
 					{
-						echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."'>";
+						if($record[self::IS_ACTIVE] == "Y")
+							echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."'>";
+						else
+							echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."' visibility='hidden'>";
 						foreach($record as $col_name => $col_data) {
 						  if($col_name != self::IS_ACTIVE)
 							echo "<td class='clickable_row'>" . $col_data . "</td>";
