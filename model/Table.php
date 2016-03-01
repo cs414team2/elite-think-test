e92707e79b97da63c628dd82f4d0716357ed0693
cs414team2 <awhiti4327@students.pcci.edu>
2016-02-29 20:00:59 -0600

    added student list for class management with checkboxes

--- model/Table.php
+++ model/Table.php
@@ -1,6 +1,5 @@
 <?php
 	class Table {
-		// Allows for a clickable
 		const IS_ACTIVE = "is_active";
 		
 		private $table_name;
@@ -17,10 +16,7 @@
 			if($statement->num_rows > 0){
 				while($record = $statement->fetch_assoc()){
 					{
-						if($record[self::IS_ACTIVE] == "Y")
-							echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."'>";
-						else
-							echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."' visibility='hidden'>";
+						echo "<tr " . "id='" . $record[$table . "_id"] . "' class='". $table . "_record, " . $record[self::IS_ACTIVE]."'>";
 						foreach($record as $col_name => $col_data) {
 						  if($col_name != self::IS_ACTIVE)
 							echo "<td class='clickable_row'>" . $col_data . "</td>";
@@ -30,7 +26,7 @@
 				}
 			}
 			else{
-				echo "<tr> <td> No Students </td> </tr>";
+				echo "<tr> <td> No " . $table_name . "s </td> </tr>";
 			}
 		}
 	}
