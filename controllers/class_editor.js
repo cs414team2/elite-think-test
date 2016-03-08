be465a4a87cb869b22d03af2fa9a3c7c2db022f2
daniel-swan <dswan0116@students.pcci.edu>
2016-03-08 11:14:07 -0600

    Started creating the code to add students to a class.

--- controllers/class_editor.js
+++ controllers/class_editor.js
@@ -5,22 +5,28 @@ const STUDENT_IS_ENROLLED = 1;
 function update_enrollment() {
 	var student = [];
 	
-	$("tr").each(function (index) {
-		student[index] = { student_id : $(this).attr("id"),
-						  is_enrolled : $(this).find("input").prop("checked") ? STUDENT_IS_ENROLLED : STUDENT_NOT_ENROLLED};
+	$("tbody").children("tr").each(function (index) {
+		student[index] = { id : $(this).attr("id"),
+				     enrolled : $(this).find("input").prop("checked") ? STUDENT_IS_ENROLLED : STUDENT_NOT_ENROLLED};
 	});
 	
-	/*$.ajax({
+	$.ajax({
 		url: "ajax/update_enrollment.php",
-		data 
-	});*/
+		type: "POST",
+		data : {
+			class_id : class_id,
+			student : student
+		},
+		success: function (data) {
+			$("#main").html(data);
+		}
+	});
 }
 
 // ******************Events*******************
 $(document).ready(function(){
 	
 	$("#btn_update_enrollment").click(function(){
-		
 		update_enrollment();
 	});
 });
