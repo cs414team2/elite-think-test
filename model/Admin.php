23c1e8c1187c4fe1f0de45c2c44f6c7c21980782
cs414team2 <awhiti4327@students.pcci.edu>
2016-03-07 20:41:16 -0600

    Fixed check boxes

--- model/Admin.php
+++ model/Admin.php
@@ -8,6 +8,7 @@
 		const TEACHER_ID    = 0;
 		const TEACHER_LNAME = 1;
 		const TEACHER_FNAME = 2;
+		const IS_ENROLLED   = 1;
 		
 		public function prepare_connection(){
 			return new mysqli("csweb.studentnet.int", "team2_cs414", "t2CS414", "cs414_team2");
@@ -32,37 +33,32 @@
 		
 		public function get_students($c_id){
 			$db = $this->prepare_connection();
-			$statement = $db->prepare("SELECT student.student_id,
-											  student.student_lname,
-											  student.student_fname,
-											  student.is_active,
-											  enrollment.class_id
-									  FROM    enrollment RIGHT JOIN student 
-									  ON      student.student_id = enrollment.student_id
-									  WHERE   class_id = ? or class_id is null");
+			$statement = $db->prepare("SELECT student_id,
+											  student_lname,
+											  student_fname,
+											  is_active,
+											  is_enrolled(student_id, ?) as enrolled
+									   FROM   student
+									   ORDER BY enrolled");
 			$statement->bind_param("i", $c_id);
 			$statement->execute();
 			$statement->store_result();
-			$statement->bind_result($student_id, $student_lname, $student_fname, $is_active, $class_id);
+			$statement->bind_result($student_id, $student_lname, $student_fname, $is_active, $is_enrolled);
 			
 			if($statement->num_rows > 0){
-				echo "<form method='post' action='#'>";
 				while($record = $statement->fetch()){
 					{
-						$class_id == $c_id ? $enrolled = "checked" : $enrolled = "";
+						$is_enrolled == self::IS_ENROLLED ? $is_enrolled = "checked" : $is_enrolled = "";
 						echo "<tr class='clickable_row'" . "id='" . $student_id . "' class='student_record, " . $is_active."'>";
 						echo "<td>
-								<div class='6u 12u(2)'>
-									<input type='checkbox' id='student_".$student_id."' ". $enrolled .">
-									<label for='student_".$student_id."'>Add</label>
-								</div>
+								<input type='checkbox' id='student_".$student_id."' ". $is_enrolled .">
+								<label for='student_".$student_id."'>Add</label>
 							  </td>";
 						echo "<td>" . $student_id . "</td>";
 						echo "<td>" . $student_lname . "</td>";
 						echo "<td>" . $student_fname . "</td>";
 						echo "</tr>\r\n";
 					}
-					echo "</form>";
 				}
 			}
 			else{
