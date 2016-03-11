e373a17bfe7ed6951d4889dd4d0436a3962ef5c0
daniel-swan <dswan0116@students.pcci.edu>
2016-03-11 12:18:43 -0600

    Created some functions for controlling the test taking.

--- controllers/test_taker.js
+++ controllers/test_taker.js
@@ -2,45 +2,45 @@ const MULTIPLE_CHOICE_QUESTION_TYPE = 'MC';
 const TRUE_FALSE_QUESTION_TYPE      = 'TF';
 const ESSAY_QUESTION_TYPE           = 'ESSAY';
 const MAX_TEST_SIZE = 3;
-const TEST_NOT_STARTED = 0;
-const TEST_STARTED     = 1;
-const TEST_COMPLETED   = 2;
-const TEST_TIMED_OUT   = 3;
+const TEST_NOT_STARTED = '0';
+const TEST_STARTED     = '1';
+const TEST_COMPLETED   = '2';
+const TEST_TIMED_OUT   = '3';
 
 //*******************Functions****************************
+
 // Check to see if a student has or has not started taking a test, or has finished taking a test.
 function check_status() {
-	/*$.ajax({
+	$.ajax({
 		url: "ajax/get_test_status.php",
-		data {
+		data: {
 			test_id: test_id,
 			student_id: student_id
 		},
-		success function(full_status){
-			
-			//Need to get the status from the html formatted status.
-			
+		success: function(status){
 			switch(status){
-				case TEST_NOT_STARTED:*/
+				case TEST_NOT_STARTED:
 					$("#btn_start").removeAttr("disabled");
 					$("#btn_complete").attr("disabled", "disabled");
-	/*				break;
+					break;
 				case TEST_STARTED:
 					$("#btn_start").attr("disabled", "disabled");
 					$("#btn_complete").removeAttr("disabled");
+					start_test(false);
 					break;
 				case TEST_COMPLETED:
 					$("#btn_start").attr("disabled", "disabled");
 					$("#btn_complete").attr("disabled", "disabled");
+					complete_test();
 					break;
 				case TEST_TIMED_OUT:
 					$("#btn_start").attr("disabled", "disabled");
 					$("#btn_complete").removeAttr("disabled");
+					disable_test();
 					break;
-					
 			}
 		}
-	});*/
+	});
 }
 
 function load_questions() {
@@ -66,20 +66,32 @@ function number_questions() {
 	});
 }
 
-function start_test() {
+function start_test(first_time) {
+	if(first_time) {
+		//Do ajax to create instance of a taking of a test and begin the timer
+		// Maybe begin the timer after the ajax returns so we can be sure it exists in the database before creating the countdown.
+	}
+	else {
+		// get the timer based on what the current time value should be (check in the database).
+	}
+	
+	load_questions();
 }
 
 function complete_test() {
 	
 }
 
+function disable_test () {
+	
+}
+
 //***********************Events************************
 $(document).ready(function(){
 	check_status();
-	load_questions();
 	
 	$("#btn_start").click(function(){
-		start_test();
+		start_test(true);
 	});
 	
 	$("#btn_complete").click(function(){
