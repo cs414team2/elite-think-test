d9365313bc00cbff1eebc094cc755493a05761ce
daniel-swan <dswan0116@students.pcci.edu>
2016-03-11 18:02:49 -0600

    Started coding the file to store student answers.

--- ajax/store_student_answers.php
+++ ajax/store_student_answers.php
@@ -1,3 +1,9 @@
 <?php
-	
+	foreach ($_REQUEST as $answer_id => $answer) {
+		echo $answer_id . " - " . $answer;
+		
+		if ($answer_id == "checkifitreallyisavalidanswercontrol") {
+			//Store $answer in the database.
+		}
+	}
 ?>
