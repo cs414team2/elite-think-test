f143041d0f79d3a207fb0245e39faf37c1510192
davis34c <jdavis2116@students.pcci.edu>
2016-03-21 21:27:33 -0500

    Small updates on some admin pages

--- views/teacher/teacherEditTest.php
+++ views/teacher/teacherEditTest.php
@@ -82,6 +82,9 @@ if (isset($_SESSION['credentials'], $_REQUEST['test_id'])) {
 						<button id="btn_open_TFDialog" class="show_hide button small fit" style="padding: 0 .5em; height: 2em; line-height: 0em;" rel="#slidingQ_2" >True / False</button>
 						<button id="btn_open_MCDialog" class="show_hide button small fit" style="padding: 0 .5em; height: 2em; line-height: 0em;" rel="#slidingQ_1" >Multiple Choice</button>
 						<button id="btn_open_EssayDialog" class="show_hide button small fit" style="padding: 0 .5em; height: 2em; line-height: 0em;" rel="#slidingQ_3" >Essay</button>
+						<br />
+						<button id="submitTest "class="show_hide button small fit" style="padding: 0 .5em; height: 2em; line-height: 0em;" >Submit Test</button>
+						
 						
 					</section>
 				</div>
