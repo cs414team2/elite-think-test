0a1d54b02f77b4f474015117d9481ae3e9171287
daniel-swan <dswan0116@students.pcci.edu>
2016-03-23 12:46:45 -0500

    Created an ajax page for getting a completed test

--- controllers/test_grader.js
+++ controllers/test_grader.js
@@ -7,5 +7,16 @@
 //*                          Events                              *
 //****************************************************************
 $(document).ready(function(){
-	
+	$('.gradeTestButton').click(function(){		
+		$.ajax({
+			url : "ajax/get_completed_test_teacher.php",
+			data : {
+				student_id : $(this).data('student-id'),
+				test_id : 1//test_id
+			},
+			success : function(test){
+				$("#test_content").html(test);
+			}
+		});
+	})
 });
--- model/Test.php
+++ model/Test.php
@@ -252,7 +252,7 @@ class Test{
 		if($statement->num_rows > 0){
 			while($statement->fetch()){
 				echo'<div class="gradeTestDiv">
-						<h1>'. $student_lname .', '. $student_fname .'<button class="alt button special reset gradeTestButton" >Grade</button><h1>
+						<h1>'. $student_lname .', '. $student_fname .'<button class="alt button special reset gradeTestButton" data-student-id="'.$student_id.'">Grade</button><h1>
 					 </div>';
 			}
 		}
