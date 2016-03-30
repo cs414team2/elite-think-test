a04aa09bfd86aeb5f2a22a27ed0dc407777c3600
davis34c <jdavis2116@students.pcci.edu>
2016-03-29 21:22:09 -0500

    Stats page updated

--- views/teacher/teacherHome.php
+++ views/teacher/teacherHome.php
@@ -4,10 +4,75 @@ if (isset($_SESSION['credentials'])) {
 	if ($_SESSION['credentials']->is_teacher()) {
 		echo '
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 		<script>
 			var user_id = '. $_SESSION['credentials']->get_user_id() .
 		';</script>
 		<script src="controllers/teacher_console.js"></script>
+		 <script type="text/javascript">
+			  google.charts.load("current", {"packages":["corechart"]});
+			  google.charts.setOnLoadCallback(drawChart);
+			  function drawChart() {
+
+				var data = google.visualization.arrayToDataTable([
+				  ["Task", "Hours per Day"],
+				  ["A\'s",     11],
+				  ["B\'s",      2],
+				  ["C\'s",  2],
+				  ["D\'s", 2],
+				  ["F\'s",    7]
+				]);
+
+				var options = {
+				  title: "Letter Grade Averages",
+				  width: 500,
+				  height: 400,
+				  backgroundColor: "#84C2CC",
+				  color: "#646464"
+				};
+
+				var chart = new google.visualization.PieChart(document.getElementById("piechart"));
+
+				chart.draw(data, options);
+			  }
+		</script>
+		<script type="text/javascript">
+			google.charts.setOnLoadCallback(drawChart1);
+			function drawChart1() {
+			  var data = google.visualization.arrayToDataTable([
+				["Element", "Missed", { role: "style" } ],
+				["Question 1", 8,  "lightblue"],
+				["Question 2", 10, "lightblue"],
+				["Question 3", 19, "lightblue"],
+				["Question 4", 21, "lightblue"]
+			  ]);
+
+			  var view = new google.visualization.DataView(data);
+			  view.setColumns([0, 1,
+							   { calc: "stringify",
+								 sourceColumn: 1,
+								 type: "string",
+								 role: "annotation" },
+							   2]);
+
+			  var options = {
+				title: "# of Students Missing Questions",
+				width: 600,
+				height: 400,
+			    backgroundColor: "#84C2CC",
+				bar: {groupWidth: "95%"},
+				legend: { position: "none" },
+			  };
+			  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+			  chart.draw(view, options);
+		  }
+		</script>		
+		
+		
+		
+		
+		
+		
 
 		<section id="main" class="wrapper style1">
 			<header class="major">
@@ -116,8 +181,11 @@ if (isset($_SESSION['credentials'])) {
 			</div>
 			
 			<!-- Test Statistics div -->
-			<div id="dlg_test_stats" title="Test Statistics" style="text-align: center;">	
-				<h2>Hey smexy</h2>			
+			<div id="dlg_test_stats" title="Test Statistics" style="text-align: center; background-color:#84C2CC">	
+				<div id="piechart" style="background-color:#84C2CC" class="chart1" ></div>	
+				<div id="columnchart_values" style="background-color:#84C2CC" class="chart2"></div>
+				<br /><br />
+				<div>Highest grade:   &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  Lowest grade: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Average grade:</div>
 			</div>
 		</section>';
 	}
