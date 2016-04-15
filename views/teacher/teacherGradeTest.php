7ec2e36f0a10d81172783b1bd6010151b366c1b7
davis34c <jdavis2116@students.pcci.edu>
2016-04-14 20:19:55 -0500

    Finalize button

--- views/teacher/teacherGradeTest.php
+++ views/teacher/teacherGradeTest.php
@@ -11,15 +11,16 @@ if (isset($_SESSION['credentials'], $_REQUEST['test_id'])) {
 				var test_id = '. $_REQUEST['test_id'] . ';
 			</script>
 			<div class="testContainer">
-				<div id="sidebar" style="text-align:center;">
+				<div id="sidebar" class="sidebar" style="text-align:center;">
 					<section style="text-align:center">
 						<h2>Select a Test to Grade </h2>
-							<section id="studentTest" style="max-height:600px; padding:1em; min-height:450px; margin-left:2em; margin-right:2em; overflow-y:auto; background-color:lightgray; ">';
+							<section id="studentTest" style="max-height:375px; padding:1em; min-height:300px; margin-left:2em; margin-right:2em; overflow-y:auto; background-color:lightgray; ">';
 								$test->get_completed_tests();
 					echo	'</section>
 					</section>
-					<br /><br />
-					<section id="test_guide" style="padding:1em; min-height:50px; margin-left:2em; margin-right:2em; background-color:lightgray; ">
+					<button class="alt fit button special" style="width:26.5em; margin:.5em;" id="btn_finalize_grade">Finalize Grade</button>
+					
+					<section id="test_guide" style="padding:1em; min-height:50px;  margin:1em; margin-left:2em; margin-right:2em; background-color:lightgray; ">
 					<h1> Grading Symbols </h1>
 						<p> &#8656; - Student gave no answer <br \> &#10003; - Student gave correct answer  <br \> x - student gave wrong answer </p>
 						
@@ -29,7 +30,6 @@ if (isset($_SESSION['credentials'], $_REQUEST['test_id'])) {
 				</div>		
 		
 				<div class="studentTest" style="float:right;">
-					<button class="alt button special" style="float:right; margin-right:2em; margin-top:1em;" id="btn_finalize_grade">Finalize Grade</button>
 					<h2 style="padding:10px;"><span id="grade_curr_stud_name"></span> Test '. $test->get_test_number() . ' - ' . $test->get_class_name() . '</h2>
 					<section id="gradeView">
 						<div id="grade_content" align="left" ';
@@ -47,6 +47,9 @@ if (isset($_SESSION['credentials'], $_REQUEST['test_id'])) {
 						}
 						
 					echo '
+						</div>
+						<div id="area_grade_loader" class="loader" style="display:none;">
+							Loading...
 						</div>
 						<br />
 					</section>
